9d2f98c95355b8aff7bdd59e0057ae2246f90e26
SimoneFilograna <simonefilogranaf12@gmail.com>
2023-09-29 15:40:53 +0200

    add-index

--- app/Http/Controllers/ComicController.php
+++ app/Http/Controllers/ComicController.php
@@ -1,10 +1,14 @@
 <?php
 
 namespace App\Http\Controllers;
-
 use Illuminate\Http\Request;
+use App\Models\Comic;
 
 class ComicController extends Controller
 {
-    //
+    public function index(){
+        $comics = Comic::all();
+
+        return view("comics.index",[ "comics" => $comics]);
+    }
 }
