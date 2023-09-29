02b12b6db65ee7ee9121569ddb8a482a1c265850
SimoneFilograna <simonefilogranaf12@gmail.com>
2023-09-29 14:38:16 +0200

    add-seed-list

--- database/migrations/2023_09_29_120049_create_comics_table.php
+++ database/migrations/2023_09_29_120049_create_comics_table.php
@@ -13,6 +13,15 @@ return new class extends Migration
     {
         Schema::create('comics', function (Blueprint $table) {
             $table->id();
+            $table->string("title");
+            $table->text("description");
+            $table->string("thumb");
+            $table->decimal("price");
+            $table->string("series")->nullable();
+            $table->date("sale_date")->nullable();
+            $table->string("type")->nullable();
+            $table->json("artists");
+            $table->json("writers");
             $table->timestamps();
         });
     }
